b97890c4d4299bc20f486bbe70bb0cc1c55fca86
ark3352 <y.dumspirospero.a@gmail.com>
2026-08-18 08:29:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="ride-height-calculator-v5";
-const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
+const CACHE="ride-height-calculator-v9";
+const ASSETS=["./","./index.html","./manifest.json","./icon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
